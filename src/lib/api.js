cb01259a8f1d1f1676a8c57cd08127452fc83b98
Alamgeer <khanalamgeer996@gmail.com>
2026-03-11 12:35:54 +0500

    change baseurl

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
+// const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE = 'https://pharma-backend-lac.vercel.app/api';
 const api = axios.create({
   baseURL: API_BASE,
   headers: { 'Content-Type': 'application/json' },
